c231e018fd75d9e818b3acef3ee145bd6ad59241
Oliver Färnskog <olive@5CG6236643.learnet.se>
2020-11-07 14:02:31 +0100

    Första commit av arbetsfilerna

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Moment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
